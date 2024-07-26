7bedb5cbaf1145dc5007efc9cbd349b98f0f36f1
musab.bozkurt <musab.bozkurt@ozan.com>
2024-07-26 15:44:02 +0300

    spring-cloud-stream-binder-kafka is added - rename EventProducerTests

--- src/test/java/com/mb/inventorymanagementservice/integration_tests/queue/producer/EventProducerIntegrationTests.java
+++ src/test/java/com/mb/inventorymanagementservice/integration_tests/queue/producer/EventProducerIntegrationTests.java
@@ -26,7 +26,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test-containers")
-public class EventProducerTests {
+public class EventProducerIntegrationTests {
 
     @Autowired
     private StreamBridge streamBridge;
